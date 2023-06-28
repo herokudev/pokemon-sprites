46b5b830e27500e3a6f7db098909cae5b5ccb16e
herokudev <ao43085@gmail.com>
2023-06-27 22:39:44 -0600

    Adding spinner
    Adding spinner

--- app/Pokemon.jsx
+++ app/Pokemon.jsx
@@ -31,7 +31,16 @@ function Pokemon({ name, url }) {
   if (error) {
     return <>{error.message}</>;
   } else if (!loaded) {
-    return <>Loading ....</>;
+    return (
+      <div>
+        <img
+          className='loading-spinner'
+          src='https://raw.githubusercontent.com/herokudev/pokemon-sprites/a8724ffe26e5c13a07f95ec7ae4900aadfcd81c7/public/pokeball.svg'
+          alt='avatar-img'
+          style={{ width: "50px", height: "50px" }}
+        ></img>
+      </div>
+    );
   } else {
     return (
       <div>
